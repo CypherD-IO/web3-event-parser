643c55c5e01b205ee2379ee273743ff35e9e8842
anupsv <anupsv@users.noreply.github.com>
2023-02-24 21:51:37 -0800

    Create events.ts

---
+++ src/handlers/uniswap/events.ts
@@ -0,0 +1,7 @@
+import { CHAIN } from "../../shared/chains.enum";
+import { BaseEventHandler, ContractDetail, EventNameWithHandler, NotificationDetail, ParsedEventData } from "../../model/baseEventHandler";
+
+export class TrasnferEventHandelr extends BaseEventHandler {
+
+
+}
